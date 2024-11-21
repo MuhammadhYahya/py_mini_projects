06894f0fecf12a243d0ea667542520f8228dece9
yahya676767 <yahya555ays@gmail.com>
2024-11-21 11:07:56 +0530

    modularized code

--- rock_paper_scissor/main.py
+++ rock_paper_scissor/main.py
@@ -4,31 +4,45 @@ ROCK = 'r'
 SCISSORS = 's'
 PAPER = 'p'
 emojis = { ROCK: '🗿', SCISSORS: '✂️', PAPER: '📃' }
-choices = ('r','p','s')
+choices = tuple(emojis.keys())
 
-while True:
-    user_input = input('Rock, paper, or scissors? (r, p, s): ').lower()
-    if user_input not in choices:
-        print('invalid input 😕')
-        continue
-
-    comp_choices=random.choice(choices)
-
-    print(f'you chose {emojis[user_input]}')
-    print(f'computer chose {emojis[comp_choices]}')
-
-    if user_input==comp_choices:
-        print('Tie! 🤝')
-    elif (
-        (user_input=='s' and comp_choices=='p') or
-        (user_input=='p' and comp_choices=='r') or
-        (user_input=='r' and comp_choices=='s')):
-        print('you win 🏆')
+def get_user_choice():
+  while True:
+    user_choice = input('Rock, paper, or scissors? (r/p/s): ').lower()
+    if user_choice in choices:
+      return user_choice      
     else:
-        print('you lose 🙁')
+      print('Invalid choice! 😕')
+      
+def display_choices(user_choice, computer_choice):
+  print(f'You chose {emojis[user_choice]}')
+  print(f'Computer chose {emojis[computer_choice]}')
 
-    should_continue=input('want to continune? (y/n) :').lower()
-    if should_continue=='n':
+
+def determine_winner(user_choice, computer_choice):
+  if user_choice == computer_choice:
+    print('Tie! 🤝')
+  elif (
+    (user_choice == ROCK and computer_choice == SCISSORS) or 
+    (user_choice == SCISSORS and computer_choice == PAPER) or 
+    (user_choice == PAPER and computer_choice == ROCK)):
+    print('You win 🏆')
+  else:
+    print('You lose 🙁')  
+
+def play_game():
+  while True:
+    user_choice = get_user_choice()
+
+    computer_choice = random.choice(choices)
+
+    display_choices(user_choice, computer_choice)
+
+    determine_winner(user_choice, computer_choice)
+
+    should_continue = input('Continue? (y/n): ').lower()
+    if should_continue == 'n':
         print("Thanks for playing! See you next time! 👋")
         break
-        
+
+play_game()
